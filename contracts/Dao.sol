291452e2819d8d94769e7066cc7dab8ae791c4ce
Oyinlolu <oyinloluwab@gmail.com>
2022-11-23 03:32:59 +0100

    first commit

---
+++ contracts/Dao.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Token.sol";
+
+interface IToken {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256);
+}
+
+contract Dao is Ownable {
+    IToken public tokenContract;
+
+    // events
+    event SubmissionCreated(string name);
+    event Vote(uint256 submissionId);
+    event Unvote(uint256 submissionId);
+
+    error voteAlreadyEnded();
+    error voteNotYetEnded();
+
+     struct Submission {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+        uint256 deadline;
+    }
+
+    mapping(uint256 => bool) voters;
+
+    Submission[] public submissionsArr;
+
+    mapping(uint256 => Submission) public submissions;
+
+    uint256 public submissionCount;
+
+    mapping(address => bool) public isVoter;
+
+    constructor(address _tokenContract) payable {
+        tokenContract = IToken(_tokenContract);
+    }
+
+    modifier tokenOwnerOnly() {
+        require(tokenContract.balanceOf(msg.sender) > 0, "You don't own any tokens");
+        _;
+    }
+
+    modifier onlyActiveProposal(uint256 submissionId) {
+        require(
+            submissions[submissionId].deadline > block.timestamp,
+            "This proposal is no longer active"
+        );
+        _;
+    }
+
+    function addSubmission(string memory _name)
+        external
+        tokenOwnerOnly
+    {
+        submissionCount++;
+        Submission memory newSubmission = Submission(
+            submissionCount,
+            _name,
+            0,
+            block.timestamp + 5 minutes
+        );
+        submissions[submissionCount] = newSubmission;
+
+        emit SubmissionCreated(_name);
+    }
+
+    function viewSubmission(uint256 _submissionId)
+        external
+        view
+        returns (Submission memory submission)
+    {
+        return submissions[_submissionId];
+    }
+
+    function vote(uint256 _submissionId)
+        external
+        tokenOwnerOnly
+        onlyActiveProposal(_submissionId)
+    {
+        // Submission storage submission = submissions[_submissionId];
+        uint256 numVotes = 0;
+
+        if (voters[_submissionId] == false) {
+            numVotes++;
+            voters[_submissionId] = true;
+        }
+
+        require(numVotes > 0, "You cannot vote twice");
+
+        isVoter[msg.sender] = true;
+        submissions[_submissionId].voteCount++;
+
+        emit Vote(_submissionId);
+    }
+
+    function viewVotes(uint256 _submissionId) public view returns (uint256) {
+        return submissions[_submissionId].voteCount;
+    }
+
+    function withdrawEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
---
+++ contracts/Token.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Token is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public max_supply = 1000 * 10 ** 18;
+
+    constructor() ERC721("Token", "MTK") {
+        _mint(msg.sender, max_supply);
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
